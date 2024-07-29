3ce734072ff2db0174d6f4ea62716afeeb6d16ec
petr <krkabol@gmail.com>
2024-07-29 15:47:56 +0200

    introduce test environment

--- htdocs/app/Bootstrap.php
+++ htdocs/app/Bootstrap.php
@@ -9,35 +9,40 @@ use Nette\Bootstrap\Configurator;
 
 class Bootstrap
 {
-	public static function boot(): Configurator
-	{
-		$configurator = new Configurator;
-		$appDir = dirname(__DIR__);
+    public static function boot(): Configurator
+    {
+        $configurator = new Configurator;
+        $appDir = dirname(__DIR__);
 
         if (getenv('NETTE_ENV', true) === 'development') {
             $configurator->setDebugMode(true);
         }
-		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
-		$configurator->enableTracy($appDir . '/log');
+        //$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
+        $configurator->enableTracy($appDir . '/log');
 
-		$configurator->setTempDirectory($appDir . '/temp');
+        $configurator->setTempDirectory($appDir . '/temp');
 
-		$configurator->createRobotLoader()
-			->addDirectory(__DIR__)
-			->register();
+        $configurator->createRobotLoader()
+            ->addDirectory(__DIR__)
+            ->register();
 
         $configurator->addStaticParameters([
             'rootDir' => realpath(__DIR__ . '/..'),
             'appDir' => __DIR__,
             'wwwDir' => realpath(__DIR__ . '/../www'),
         ]);
-        if (getenv('NETTE_ENV', true) === 'development') {
-            $configurator->addConfig($appDir . '/config/env/dev.neon');
-        } else {
-            $configurator->addConfig($appDir . '/config/env/prod.neon');
+        $environment =getenv('NETTE_ENV', true);
+        switch ($environment) {
+            case "development":
+                $configurator->addConfig($appDir . '/config/env/dev.neon');
+                break;
+            case "test":
+                $configurator->addConfig($appDir . '/config/env/test.neon');
+                break;
+            default:
+                $configurator->addConfig($appDir . '/config/env/prod.neon');
         }
         $configurator->addConfig($appDir . '/config/local.neon');
-
         return $configurator;
-	}
+    }
 }
