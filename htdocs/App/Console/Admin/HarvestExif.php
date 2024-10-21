bacb9bd1b8770a44fdd16638ae2d980987f09c62
Petr Novotný <krkabol@gmail.com>
2024-10-21 15:02:34 +0200

    add try/catch

--- htdocs/App/Console/Admin/HarvestExif.php
+++ htdocs/App/Console/Admin/HarvestExif.php
@@ -15,8 +15,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 class HarvestExif extends Command
 {
 
-    protected const string TEMPNAME = DIRECTORY_SEPARATOR."exif.tif";
-    public function __construct(protected readonly EntityManager $entityManager,  protected readonly CuratorFacade $curatorService, protected readonly TempDir $tempDir, protected readonly ImageService $imageService, ?string $name = null)
+    protected const string TEMPNAME = DIRECTORY_SEPARATOR . "exif.tif";
+
+    public function __construct(protected readonly EntityManager $entityManager, protected readonly CuratorFacade $curatorService, protected readonly TempDir $tempDir, protected readonly ImageService $imageService, ?string $name = null)
     {
         parent::__construct($name);
     }
@@ -25,7 +26,7 @@ class HarvestExif extends Command
     {
         $rsm = new ResultSetMappingBuilder($this->entityManager);
         $rsm->addRootEntityFromClassMetadata('App\Model\Database\Entity\Photos', 'p');
-        $query = $this->entityManager->createNativeQuery('SELECT p.* FROM photos p WHERE status_id IN (?) AND identify is null ORDER BY id asc', $rsm);
+        $query = $this->entityManager->createNativeQuery('SELECT p.* FROM photos p WHERE status_id IN (?) AND identify is null ORDER BY id asc LIMIT 1000', $rsm);
         $query->setParameter(1, PhotosStatus::PASSED);
         return $query->execute();
     }
@@ -36,7 +37,7 @@ class HarvestExif extends Command
         $this->setDescription('harvest Exif and Identify metadata from images uploaded before the repository was established');
     }
 
-    protected function tempFile():string
+    protected function tempFile(): string
     {
         return $this->tempDir->getPath(self::TEMPNAME);
     }
@@ -44,20 +45,25 @@ class HarvestExif extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $startTime = microtime(true);
-        foreach ($this->getListOfPhotos() as $photo) {
-            $output->write("\n photoId: " . $photo->getId() . "\n");
-            $this->curatorService->getArchiveFile($photo, $this->tempFile());
+        try {
+            foreach ($this->getListOfPhotos() as $photo) {
+                $output->write("\n photoId: " . $photo->getId() . "\n");
+                $this->curatorService->getArchiveFile($photo, $this->tempFile());
 
-            $imagick = $this->imageService->createImagick($this->tempFile());
-            $photo->setIdentify($this->imageService->readIdentify($imagick));
-            $photo->setExif($this->imageService->readExif($imagick));
-            $imagick->destroy();
-            unset($imagick);
+                $imagick = $this->imageService->createImagick($this->tempFile());
+                $photo->setIdentify($this->imageService->readIdentify($imagick));
+                $photo->setExif($this->imageService->readExif($imagick));
+                $imagick->destroy();
+                unset($imagick);
 
-            $photo->setLastEditAt();
-            $this->entityManager->flush();
-            unlink($this->tempFile());
+                $photo->setLastEditAt();
+                $this->entityManager->flush();
+                unlink($this->tempFile());
+            }
+        } catch (\Throwable $throwable) {
+            $output->writeln("\n Error: " . $throwable->getMessage());
         }
+
         $output->writeln(sprintf("\n Execution time: %.2f sec", (microtime(true) - $startTime)));
         return Command::SUCCESS;
     }
